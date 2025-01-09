3a61a61f285c1e27a2abbf1c6b4a04e7e8f12d0d
yewonming <kimyewon120891@gmail.com>
2025-01-09 23:20:48 +0900

    day03

---
+++ day03/day03-2.c
@@ -0,0 +1,52 @@
+﻿
+
+1.7 목록과 표 형식을 위한 태그
+<ul> -순서 없는 목록
+<ul>
+<li>Coffee< / li>
+<li>Tea< / li>
+<li>Milk< / li>
+< / ul>
+<ol>순서 있는 목록
+<ol>
+<li>Coffee< / li>
+<li>Tea< / li>
+<li>Milk< / li>
+< / ol>
+
+-type 
+어트리뷰트를 사용하여 순서를 나타내는 문자를 지정할 수 있음
+“1”숫자(기본값)
+“A”대문자 알파벳
+“a”소문자 알파벳
+“I”대문자 로마숫자
+“i”소문자 로마숫자
+
+- start
+시작값 지정
+<ol start = "3">
+출력: 3, 4, 5 번호순으로 나옴
+
+- reversed
+순서값을 역으로 표현
+<ol reversed>
+출력 : 3, 2, 1 번호순으로 출력
+
+- 테이블
+표를 만들때 사용하는 태그
+table : 표를 감싸는 태그
+tr : 표 내부의 행(table row)
+th : 행 내부의 제목 셀(table heading)
+td : 행 내부의 일반 셀(table data)
+
+* 가로로 표를 만든다고 생각하고 하면 쉬움 
+
+어트리뷰트
+border : 표 테두리 두께 지정.
+rowspan해당 셀이 점유하는 행의 수 지정
+colspan해당 셀이 점유하는 열의 수 지정
+
+
+
+
+
